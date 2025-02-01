df357f2d3f40d23c43e018864ed021791925f58d
VarK2025 <vk.panthers@gmail.com>
2025-02-01 18:35:39 -0500

    Update main.py

--- Assignment 4-2/main.py
+++ Assignment 4-2/main.py
@@ -4,8 +4,16 @@ numTransactions = input("Enter Number of Transactions: "))
 transactionValue = input("Enter Transaction dollar value: ")
 
 productivityScore = transactionValue / numTransactions / numShifts
+bonus = 0.0
 
 if productivityScore >= 200:
+  bonus = 200.00
+elif productivityScore >= 70:
+  bonus = 100.0
+elif productivityScore >= 31:
+  bonus = 75.0
+else:
+  bonus = 50.0
 
 print("Employee Name: " + name)
 print("Employee Bonus: $" + float(bonus))
