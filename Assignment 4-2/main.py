59a213218f54e2b3a5911d397bb8d33b014b9698
VarK2025 <vk.panthers@gmail.com>
2025-02-01 18:36:41 -0500

    Update main.py

--- Assignment 4-2/main.py
+++ Assignment 4-2/main.py
@@ -1,6 +1,6 @@
 name = input("Enter Employee's name: ")
 numShifts = int(input("Enter Number of Shifts: "))
-numTransactions = input("Enter Number of Transactions: "))
+numTransactions = input("Enter Number of Transactions: ")
 transactionValue = input("Enter Transaction dollar value: ")
 
 productivityScore = transactionValue / numTransactions / numShifts
