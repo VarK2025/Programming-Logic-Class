83baa020928fc4ffcc5a0b362dd647b5be0ff690
VarK2025 <vk.panthers@gmail.com>
2025-03-23 15:03:39 -0400

    Create main.py

---
+++ AutoCountry Project/Project 0-1/main.py
@@ -0,0 +1,32 @@
+AllowedVehiclesList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ] # Program dataset
+
+# Function that is run upon execution of the program
+def onLoad():
+    print("********************************")
+    print("AutoCountry Vehicle Finder v0.1")
+    print("********************************")
+    print("Please Enter the following number below from the following menu:\n")
+    print("1. PRINT all Authorized Vehicles")
+    print("2. Exit\n")
+    user_input = input()
+    print("")
+
+    match user_input:
+
+        case "1":
+            printVehicles()
+
+        case "2":
+            exitProgram()
+
+def printVehicles():
+    print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+    for car in AllowedVehiclesList:
+        print(car)
+    print("")
+    onLoad()
+
+def exitProgram():
+    print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+
+onLoad()
